b5c1704fd7ed6db99d73625b8980e2ec8b9ccb44
joizhang <zxlwhom@outlook.com>
2019-09-29 21:43:44 +0800

    chore: Delete weekly contest 155;

--- Weekly contest 155/TEST.cpp
+++
@@ -1,9 +0,0 @@
-#define CATCH_CONFIG_MAIN
-#include "../Catch2/single_include/catch2/catch.hpp"
-#include "q2.h"
-
-TEST_CASE("155") {
-    Solution s;
-    int n = 1000000000, a = 2, b = 217983653, c = 336916467;
-    REQUIRE(s.nthUglyNumber(n, a, b, c) == 1999999984);
-}
--- Weekly contest 155/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main() {
-    cout << INT_MAX << endl;
-    return 0;
-}
